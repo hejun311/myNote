972e4b8a9ade9c7e9a9cd41a248374aee999c09e
hejun <jun.he@ubtrobot.com>
2022-07-11 16:56:27 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  dest:'public',
   title: 'Lynn\'s blog',
   description: '我的个人网站',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
   base: '/', // 这是部署到github相关的配置
   markdown: {
